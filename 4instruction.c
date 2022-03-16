dc57f3e08b2cbf49e76252b6d7308a4d4b4f406d
Ravi-29 <84237488+Ravi-29@users.noreply.github.com>
2022-03-16 14:29:25 +0530

    Add files via upload

---
+++ 4instruction.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+
+{
+    printf("the value of4+4 is %d\n",4+4);
+    printf("the value of 3+3.4 is %.1 f\n",3+3.4);
+     printf("the value of3.5 +3.5 is %.2f\n",3.5+3.5);
+     return 0;
+}
