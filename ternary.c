78c22b43e2142f99cf14c9cd734659a225592689
Ravi-29 <84237488+Ravi-29@users.noreply.github.com>
2022-03-18 17:45:50 +0530

    Add files via upload

---
+++ ternary.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    int a;
+    printf("enter a\n");
+    scanf("d", &a);
+
+    // on liner if statement
+    (a<5)? printf("A is less than 5"): printf("A is not less than 5");
+    return 0;
+}
