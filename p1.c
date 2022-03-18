2a5c3d73a8493a20185269ecb6f80807b4dc9daa
Ravi-29 <84237488+Ravi-29@users.noreply.github.com>
2022-03-18 20:52:11 +0530

    Add files via upload

---
+++ p1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    // int a,b;
+     float sum;
+    // scanf("%d, %d", &a,&b);
+    // printf("the sum of a+b is %d\n", a+b);
+    // printf("power of a and b is %f\n", pow(a,b));
+
+    sum = 5/2;
+    printf("the value of sum is %f\n", sum);
+
+    return 0;
+}
