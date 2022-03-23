2557a8865d4604b27e0d1448d7243c8115790572
Ravi Shankar <84237488+Ravi-29@users.noreply.github.com>
2022-03-23 23:17:10 +0530

    Add files via upload

---
+++ 5dasg.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+
+int main(){
+
+    int rating;
+     printf("rating\n");
+     scanf("%d",&rating);
+
+     switch(rating)
+     {
+     case 1:
+      printf("rating is 1\n");
+      break;
+
+     case 2:
+      printf("rating is 2\n");
+      break;
+
+     case 3:
+      printf("rating is 3\n");
+      break;
+
+     case 4:
+      printf("rating is 4\n");
+      break;
+
+     case 5:
+      printf("rating is 5\n");
+      break;
+
+     case 6:
+      printf("rating is 6\n");
+      break;
+
+     case 7:
+      printf("rating is 7\n");
+      break;
+
+     case 8:
+      printf("rating is 8\n");
+      break;
+
+     case 9:
+      printf("rating is 9\n");
+      break;
+
+     case 10:
+      printf("rating is 10\n");
+      break;
+
+     
+
+      return 0;
+     }
+}
