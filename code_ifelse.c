72831c91d1af7fe7334f0e816bb5d1eff60ee35a
Ravi Shankar <84237488+Ravi-29@users.noreply.github.com>
2022-03-25 11:05:59 +0530

    Add files via upload

---
+++ code_ifelse.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+
+{
+    int X;
+    printf("X");
+    scanf("%d",&X);
+
+    if(X>=750){
+        printf("Yes");
+    }
+    else{
+        printf("No");
+    }
+    return 0;
+
+}
