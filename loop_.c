6dcbee59225d393dea10a80a8d1e7424f9716f62
Ravi Shankar <84237488+Ravi-29@users.noreply.github.com>
2022-04-05 11:48:34 +0530

    Add files via upload

---
+++ loop_.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+
+{
+    // loops are used to repeat similar part of a code 
+    int a=1;
+    printf("%d\n",a);
+    a++;
+    printf("%d\n",a);
+    a++;
+    printf("%d\n",a);
+    a++;
+    return 0;
+
+}
