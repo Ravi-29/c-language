f9b45428a95a27cb7cbd07cbbcdedb481964baa8
Ravi Shankar <84237488+Ravi-29@users.noreply.github.com>
2022-04-06 19:53:28 +0530

    Add files via upload

---
+++ loop_quiz.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+    {
+        int a;
+        scanf("%d", &a);
+        while(a<=20){
+            printf("the value of a is %d\n", a);
+            a++;
+        }
+    return 0;
+    }
